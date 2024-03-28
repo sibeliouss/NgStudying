5bf85f762e91d8751cd2091a74bc628694dfa497
sibzey <sibelmozturk@hotmail.com>
2024-03-28 22:40:27 +0300

    event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'NgStudying';
+  count = 0;
+  inputValue: string = '';
+  onBtnClick(event: Event) {
+    console.log(this.inputValue);
+    this.count++;}
+
 }
