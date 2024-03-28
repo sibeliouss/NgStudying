cc586c2d8b52a3b9e738856b8303c7825c68adc4
sibzey <sibelmozturk@hotmail.com>
2024-03-29 00:12:25 +0300

    promise

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
-export class HomepageComponent {
+export class HomepageComponent  {
+  todoList: string[]=[];
+  constructor(private httpClient: HttpClient){}
+  ngOnInit():void{
+this.getTodos();
+  }
+  getTodos(){
+    console.log('Fonksiyon öncesi');
+    this.someAsyncOperation()
+    .then((response:string)=>{
+      console.log('cevap başatılı:',response);
+    
+    })
+    .catch((error) => {
+      console.log('Cevap başarısız promiseden şu değer geldi:', error);
+    }) // işlem başarısız
+    .finally(() => {
+      console.log('Promise işlemi başarılı ya da başarısız sonlandı.');
+    }); // işlem bitti (başarılı-başarısız)
+  // then-catch-finally
+  setTimeout(() => {
+    console.log('Fonksiyon sonrası');
+  }, 4000);
+  console.log('Sync');
 
-}
+  }
+  async getTodos2() {
+    try {
+      let value: string = await this.someAsyncOperation();
+      console.log(value);
+    } catch (error) {
+      console.log('Hata:', error);
+    }
+  }
+
+  someAsyncOperation(): Promise<string>{
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve('123456');
+      },2000);
+    });
+  }
+
+  }
+
+
