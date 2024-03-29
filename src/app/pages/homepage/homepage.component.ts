82c4062d296d89e25dceb89f82e441bc0bec77f5
sibzey <sibelmozturk@hotmail.com>
2024-03-29 12:22:37 +0300

    httpclient

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ToDo } from '../../models/ToDo';
 
 @Component({
   
@@ -10,47 +11,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent  {
-  todoList: string[]=[];
+  todoList: ToDo[]=[];
   constructor(private httpClient: HttpClient){}
   ngOnInit():void{
 this.getTodos();
   }
   getTodos(){
-    console.log('Fonksiyon öncesi');
-    this.someAsyncOperation()
-    .then((response:string)=>{
-      console.log('cevap başatılı:',response);
+  //   console.log('Fonksiyon öncesi');
+  //   this.someAsyncOperation()
+  //   .then((response:string)=>{
+  //     console.log('cevap başatılı:',response);
     
-    })
-    .catch((error) => {
-      console.log('Cevap başarısız promiseden şu değer geldi:', error);
-    }) // işlem başarısız
-    .finally(() => {
-      console.log('Promise işlemi başarılı ya da başarısız sonlandı.');
-    }); // işlem bitti (başarılı-başarısız)
-  // then-catch-finally
-  setTimeout(() => {
-    console.log('Fonksiyon sonrası');
-  }, 4000);
-  console.log('Sync');
+  //   })
+  //   .catch((error) => {
+  //     console.log('Cevap başarısız promiseden şu değer geldi:', error);
+  //   }) // işlem başarısız
+  //   .finally(() => {
+  //     console.log('Promise işlemi başarılı ya da başarısız sonlandı.');
+  //   }); // işlem bitti (başarılı-başarısız)
+  // // then-catch-finally
+  // setTimeout(() => {
+  //   console.log('Fonksiyon sonrası');
+  // }, 4000);
+  // console.log('Sync');
+
+  this.httpClient
+  .get<ToDo[]>('https://jsonplaceholder.typicode.com/todos')
+  .subscribe({
+    next:(response: ToDo[])=>{
+      console.log('Backendten cvp geldi.',response);
+      this.todoList=response;
+    },
+    error:(error)=>{console.log("Backendten htalı cevap geldi.",error);},
+    complete:()=>{console.log('backend isteği sonlandı.');}
+  
+
+  });
 
   }
-  async getTodos2() {
-    try {
-      let value: string = await this.someAsyncOperation();
-      console.log(value);
-    } catch (error) {
-      console.log('Hata:', error);
-    }
-  }
+  //async getTodos2() {
+   // try {
+     // let value: string = await this.someAsyncOperation();
+      //console.log(value);
+    //} catch (error) {
+      //console.log('Hata:', error);
+    //}
+  //}
 
-  someAsyncOperation(): Promise<string>{
-    return new Promise((resolve,reject)=>{
-      setTimeout(()=>{
-        resolve('123456');
-      },2000);
-    });
-  }
+  // someAsyncOperation(): Promise<string>{
+  //   return new Promise((resolve,reject)=>{
+  //     setTimeout(()=>{
+  //       resolve('123456');
+  //     },2000);
+  //   });
+  // }
+  postToDo(){let obj={};
+this.httpClient.post('link',obj).subscribe();}
 
   }
 
